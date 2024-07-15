1df8380d0f3cbda373af32ab5ba752391c71f005
junyan li <lijy1@lenovo.com>
2024-07-15 14:10:00 +0800

    rename readme.md

--- get-WifiPasswords.ps1
+++ get-WifiPasswords.ps1
@@ -1,7 +1,7 @@
 # Save the following script to a file named Get-WiFiPasswords.ps1
 $wifiProfiles = netsh wlan show profiles | Select-String "All User Profile" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
 
-foreach ($profile in $wifiProfiles) {
+$wifiPasswords = foreach ($profile in $wifiProfiles) {
     $profileInfo = netsh wlan show profile name="$profile" key=clear
     $keyContent = $profileInfo | Select-String "Key Content" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
     [PSCustomObject]@{
@@ -10,5 +10,3 @@ foreach ($profile in $wifiProfiles) {
     }
 }
 
-# Run the script in PowerShell
-# .\Get-WiFiPasswords.ps1
