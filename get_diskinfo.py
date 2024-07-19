beff7b3c699194b85e7df1d77c33343fd6a8f922
junyan li <lijy1@lenovo.com>
2024-07-19 15:02:17 +0800

    remove comment data

--- get_diskinfo.py
+++ get_diskinfo.py
@@ -4,11 +4,6 @@ def getDiskInformation():
     disk_info = psutil.disk_partitions(all=True)
     for disk in disk_info:
         print(f"Device: {disk.device}, MountPoint: {disk.mountpoint}, FileSystemType: {disk.fstype}, Options: {disk.opts}")
-        # print("Device:", disk.device)
-        # print("Mountpoint:", disk.mountpoint)
-        # print("File system type:", disk.fstype)
-        # print("Options:", disk.opts)
-        # print("")
 
 
 def main():
